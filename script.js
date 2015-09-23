b8808a7d7261861b2657bb02f0e9aeff007efe0b
mukesh <mukesh@gmail.com>
2015-09-23 08:30:43 +0530

    UI changes

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 global.$ = $;
 
+global.quickJumpList = [];
 var remote = require('remote');
 var Menu = remote.require('menu');
 var BrowserWindow = remote.require('browser-window');
 var MenuItem = remote.require('menu-item');
+var path = remote.require('path');
 var shell = require('shell');
 
 var abar = require('address_bar');
 var folder_view = require('folder_view');
+var mime = remote.require('mime');
 
-// $('title').text('hi there');
+
+// adds an element to the array if it does not already exist using a comparer
+// function
+Array.prototype.pushUnique = function(element) {
+	if (this.indexOf(element) === -1) {
+		this.push(element);
+	}
+};
+
+
 
 // append default actions to menu for OSX
 var initMenu = function() {
@@ -59,6 +71,8 @@ var App = {
 		if (path.indexOf('~') === 0) {
 			path = path.replace('~', process.env.HOME);
 		}
+
+		quickJumpList.pushUnique(path);
 		this.folder.open(path);
 		this.addressbar.set(path);
 	}
@@ -70,14 +84,19 @@ $(document).ready(function() {
 	var folder = new folder_view.Folder($('#files'));
 	var addressbar = new abar.AddressBar($('#addressbar'));
 
-	folder.open('/home/mukesh/');
-	addressbar.set('/home/mukesh/');
+	userhome = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+	folder.open(userhome);
+	addressbar.set(userhome);
 
 	App.folder = folder;
 	App.addressbar = addressbar;
 
+
 	folder.on('navigate', function(dir, mime) {
+		$('#context_dropdown').hide();
 		if (mime.type == 'folder') {
+			console.log(JSON.stringify(mime));
+			quickJumpList.pushUnique(mime.path);
 			addressbar.enter(mime);
 		} else {
 			shell.openItem(mime.path);
@@ -94,7 +113,50 @@ $(document).ready(function() {
 		App.cd(this);
 	});
 	initTypeAhead();
+
+	$('body').on('keydown', function(e) {
+		if (e.ctrlKey) {
+			switch (e.which) {
+				case 80:
+					e.preventDefault();
+					typeahead_elem.focus();
+					break;
+				case 67:
+					// emit event
+					e.preventDefault();
+					console.log('copy...');
+					break;
+				default:
+
+			}
+		}
+
+		// make sure that tt menu is not active FIXME
+		var current = $(this).find('.focus');
+		var file_path = current.attr('data-path');
+
+		var newelem = [];
+		if (e.which == 40) {
+			newelem = current.next();
+		} else if (e.which == 38) {
+			newelem = current.prev();
+		} else if (e.which == 13) { //enter
+			folder.emit('navigate', file_path, mime.stat(file_path));
+		}
+
+		if (newelem.length > 0) {
+			e.preventDefault();
+			newelem.addClass('focus');
+			current.removeClass('focus');
+			newelem.scrollintoview();
+		}
+	});
+
+	attachDragger();
+
+
 });
+
 
 function initTypeAhead() {
 	var substringMatcher = function(strs) {
@@ -116,40 +178,33 @@ function initTypeAhead() {
 		};
 	};
 
-	var quickJumpList = [ ];
 
 	var typeahead_elem = $('#the-basics .typeahead');
 	typeahead_elem.typeahead({
 		hint: true,
 		highlight: true,
 		autoselect: true,
-		minLength: 0
+		minLength: 0,
+
 	}, {
 		name: 'quickJumpList',
-		source: substringMatcher(quickJumpList)
+		source: substringMatcher(quickJumpList),
+
+		templates: {
+			empty: [
+				'<div class="empty-message">',
+				'unable to find any matches',
+				'</div>'
+			].join('\n'),
+
+			suggestion: function(data) {
+				var jsonpath = path.parse(data);
+				return '<div style="border-bottom:solid 1px #AAA;"><strong> ' + jsonpath.base + '</strong><br><span class="small">' + path.join(jsonpath.dir,jsonpath.base)+ '</span>  </div>';
+			}
+		},
+
 	});
 
-
-	$('body').on('keydown', function(e) {
-		e.preventDefault();
-		if (e.ctrlKey && event.which == 80) // ctrl-p
-			typeahead_elem.focus();
-
-
-		if (e.which == 40 || e.which == 38) {
-			var current = $(this).find('.focus');
-			if (e.which == 40) {
-				newelem = current.next();
-			} else if (e.which == 38) {
-				newelem = current.prev();
-			}
-			if (newelem.length > 0) {
-				newelem.addClass('focus');
-				current.removeClass('focus');
-				newelem.scrollintoview();
-			}
-		}
-	});
 
 	typeahead_elem.bind('focus', function() {
 		$(this).val('');
@@ -158,4 +213,12 @@ function initTypeAhead() {
 		App.setPath($(this).val());
 		$(this).blur();
 	});
+
+}
+
+function attachDragger() {
+	// dragula({
+	// 	containers: [document.getElementById('taskstack'), document.getElementById('files')],
+	// 	copy: true
+	// });
 }
